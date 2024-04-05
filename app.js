541a15974c9e9d8689fb1924ad20298fcd80c9f4
Johnathan <jonathanhas9@gmail.com>
2024-04-05 00:36:00 -0400

    Add CORS middleware

--- app.js
+++ app.js
@@ -1,43 +1,50 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var mailRouter = require('./routes/mail');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var mailRouter = require("./routes/mail");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/mail', mailRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/mail", mailRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
+
+var corsOptions = {
+  origin: process.env.WEB_URL,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
 
 module.exports = app;
--- routes/mail.js
+++ routes/mail.js
@@ -1,34 +1,36 @@
 var express = require("express");
 var router = express.Router();
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 router.get("/:username", async function (req, res, next) {
-    const userId = await prisma.user.findUnique({
-        where: {
-            username: req.params.username,
-        },
-        select: {
-            id: true
-        }
-    }).then(data => data.id);
-
-   const mail = await prisma.mail.findMany({
-        where: {
-            OR: [
-                {
-                    senderId: userId
-                },
-                {
-                    recipientId: userId
-                }
-            ]
-        },
-        select: {
-            id: true,
-            body: true
-        }
+  const userId = await prisma.user
+    .findUnique({
+      where: {
+        username: req.params.username,
+      },
+      select: {
+        id: true,
+      },
     })
+    .then((data) => data.id);
+
+  const mail = await prisma.mail.findMany({
+    where: {
+      OR: [
+        {
+          senderId: userId,
+        },
+        {
+          recipientId: userId,
+        },
+      ],
+    },
+    select: {
+      id: true,
+      body: true,
+    },
+  });
   res.send(mail);
 });
 
